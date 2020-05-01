f06755d9f812af606ba05b85bc20c2069bcd7395
Mattéo <60100878+Matteo-Ynov@users.noreply.github.com>
2020-05-01 23:53:38 +0200

    Added connexion detection

--- menu.py
+++ menu.py
@@ -6,78 +6,78 @@ from snake_client import snake_client
 from tic_tac_toe_client import tic_tac_toe_client
 from saloon import show_saloon
 
+if pseudo != None:
+    bg = pygame.image.load("menu_sprites/menu.png")
 
+    hover = pygame.image.load("menu_sprites/hover.png")
 
-bg = pygame.image.load("menu_sprites/menu.png")
+    clock = pygame.time.Clock()
 
-hover = pygame.image.load("menu_sprites/hover.png")
+    btn_width = 487
+    btn_height = 71
 
-clock = pygame.time.Clock()
+    win = pygame.display.set_mode((600, 600))
+    pygame.display.set_caption("PyNetGames")
+    pygame.font.init()
+    font = pygame.font.Font("fonts/PixelOperatorMono8-Bold.ttf", 20)
+    welcome_text = font.render(f"Welcome, {pseudo} !", 1, (255, 255, 255))
 
-btn_width = 487
-btn_height = 71
+    x_hover, y_hover = (None, None)
 
-win = pygame.display.set_mode((600, 600))
-pygame.display.set_caption("PyNetGames")
-pygame.font.init()
-font = pygame.font.Font("fonts/PixelOperatorMono8-Bold.ttf", 20)
-welcome_text = font.render(f"Welcome, {pseudo} !", 1, (255, 255, 255))
+    menu = True
+    while menu:
+        clock.tick(60)
+        win.blit(bg, (0, 0))
 
-x_hover, y_hover = (None, None)
+        win.blit(welcome_text, (600 / 2 - welcome_text.get_width() // 2, 15))
 
-menu = True
-while menu:
-    clock.tick(60)
-    win.blit(bg, (0, 0))
+        x_mouse, y_mouse = pygame.mouse.get_pos()
+        for e in pygame.event.get():
+            if e.type == QUIT:
+                menu = False
+                break
 
-    win.blit(welcome_text, (600 / 2 - welcome_text.get_width() // 2, 15))
+            if (
+                x_mouse > 56
+                and x_mouse < 56 + btn_width
+                and y_mouse > 207
+                and y_mouse < 207 + btn_height
+            ):
+                x_hover, y_hover = (56, 207)
+                if e.type == MOUSEBUTTONDOWN and e.button == 1:
+                    show_saloon("Pong", pseudo)
+            elif (
+                x_mouse > 56
+                and x_mouse < 56 + btn_width
+                and y_mouse > 292
+                and y_mouse < 292 + btn_height
+            ):
+                x_hover, y_hover = (56, 292)
+                if e.type == MOUSEBUTTONDOWN and e.button == 1:
+                    show_saloon("Tic_Tac_Toe", pseudo)
+            elif (
+                x_mouse > 56
+                and x_mouse < 56 + btn_width
+                and y_mouse > 377
+                and y_mouse < 377 + btn_height
+            ):
+                x_hover, y_hover = (56, 377)
+                if e.type == MOUSEBUTTONDOWN and e.button == 1:
+                    show_saloon("Snake", pseudo)
+            elif (
+                x_mouse > 56
+                and x_mouse < 56 + btn_width
+                and y_mouse > 462
+                and y_mouse < 462 + btn_height
+            ):
+                x_hover, y_hover = (56, 462)
+                if e.type == MOUSEBUTTONDOWN and e.button == 1:
+                    print("Uno")
+            else:
+                x_hover, y_hover = (None, None)
 
-    x_mouse, y_mouse = pygame.mouse.get_pos()
-    for e in pygame.event.get():
-        if e.type == QUIT:
-            menu = False
-            break
+        if x_hover and y_hover:
+            win.blit(hover, (x_hover, y_hover))
 
-        if (
-            x_mouse > 56
-            and x_mouse < 56 + btn_width
-            and y_mouse > 207
-            and y_mouse < 207 + btn_height
-        ):
-            x_hover, y_hover = (56, 207)
-            if e.type == MOUSEBUTTONDOWN and e.button == 1:
-                show_saloon("Pong", pseudo)
-        elif (
-            x_mouse > 56
-            and x_mouse < 56 + btn_width
-            and y_mouse > 292
-            and y_mouse < 292 + btn_height
-        ):
-            x_hover, y_hover = (56, 292)
-            if e.type == MOUSEBUTTONDOWN and e.button == 1:
-                show_saloon("Tic_Tac_Toe", pseudo)
-        elif (
-            x_mouse > 56
-            and x_mouse < 56 + btn_width
-            and y_mouse > 377
-            and y_mouse < 377 + btn_height
-        ):
-            x_hover, y_hover = (56, 377)
-            if e.type == MOUSEBUTTONDOWN and e.button == 1:
-                show_saloon("Snake", pseudo)
-        elif (
-            x_mouse > 56
-            and x_mouse < 56 + btn_width
-            and y_mouse > 462
-            and y_mouse < 462 + btn_height
-        ):
-            x_hover, y_hover = (56, 462)
-            if e.type == MOUSEBUTTONDOWN and e.button == 1:
-                print("Uno")
-        else:
-            x_hover, y_hover = (None, None)
+        pygame.display.update()
 
-    if x_hover and y_hover:
-        win.blit(hover, (x_hover, y_hover))
-
-    pygame.display.update()
