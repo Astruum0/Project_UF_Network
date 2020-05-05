85dd0bfd00736b6a74351ec7f3205fe03d4f7a1b
Mattéo <60100878+Matteo-Ynov@users.noreply.github.com>
2020-05-05 16:43:18 +0200

    Added virtual env

--- launcher.py
+++ launcher.py
@@ -2,6 +2,9 @@ from tkinter import *
 from tkinter.font import Font
 from tkinter import ttk
 from database import Database
+import re
+import smtplib
+import email.mime.text
 
 width = 600
 mysql_config = {
@@ -31,7 +34,7 @@ def connected(database):
     window.geometry(f"{width}x{width}")
     window.configure(background="black")
     
-    def login():
+    def login(*arg):
         global pseudo
         l_username = username.get()
         l_password = password.get()
@@ -64,15 +67,20 @@ def connected(database):
 
     def register():
         global pseudo
+        regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
         r_user_name = r_username.get()
         r_password1 = r_password.get()
         r_password2 = r_conf_password.get()
-        if r_user_name and r_password1 and r_password2:
+        r_email = mail_entry.get()
+        if r_user_name and r_password1 and r_password2 and r_email:
             if r_valid_username(r_user_name):
                 if valid_password(r_password1, r_password2):
-                    inscription(r_user_name, r_password1)
-                    pseudo = r_user_name
-                    window.destroy()
+                    if(re.search(regex,r_email)): 
+                        inscription(r_user_name, r_password1, r_email)
+                        pseudo = r_user_name
+                        window.destroy()
+                    else:
+                        error_register.config(text="Email invalide", background="grey")
             else:
                 error_register.config(text="Ce nom est déjà pris", background="grey")
         else:
@@ -99,25 +107,26 @@ def connected(database):
         except:
             launch(window)
 
-    def inscription(user_name, user_password):
-        #try:
-        SQL_INSERT = "INSERT INTO {table}({columns}) VALUES ({placeholders})"
-        colonne = ("user_name", "user_password", "win_snake", "win_ttt", "win_pong")
-        table = "users"
-        query = SQL_INSERT.format(
-            columns=",".join(colonne),
-            table=table,
-            placeholders=",".join(["%s" for i in range(len(colonne))]),
-        )
-        database.post(query, (user_name, user_password, 0, 0, 0),)
-        #except:
-        #    launch(window)
+    def inscription(user_name, user_password, email):
+        try:
+            SQL_INSERT = "INSERT INTO {table}({columns}) VALUES ({placeholders})"
+            colonne = ("user_name", "user_password", "email", "win_snake", "win_ttt", "win_pong")
+            table = "users"
+            query = SQL_INSERT.format(
+                columns=",".join(colonne),
+                table=table,
+                placeholders=",".join(["%s" for i in range(len(colonne))]),
+            )
+            database.post(query, (user_name, user_password, email, 0, 0, 0),)
+        except:
+            launch(window)
+
 
     error_login = Label(window, font=("PixelOperator8",15), text="", background ="black")
-    error_login.place(x=50, y=300)
+    error_login.place(x=70, y=350)
     error_register = Label(window, font=("PixelOperator8",15), text="", background ="black")
-    error_register.place(x=350, y=300)
-        
+    error_register.place(x=370, y=350)
+    window.bind('<Return>', login)
     ### Partie Login    
     log = Label(window, font=("PixelOperator8",40), text="Log In", width=7)
     log.place(x=38, y=50)
@@ -133,7 +142,10 @@ def connected(database):
     password.place(x=135, y=190)
     #Bouton
     login = Button(window, text="Login", command=login, width=10)
-    login.place(x=125, y=400)
+    login.place(x=50, y=430)
+    #Bouton mdp oublié
+    login = Button(window, text="Mdp oublié ?", command= lambda data = database, win = window : mdp(data, win), width=10)
+    login.place(x=175, y=430)
 
     ###Partie Register
     reg = Label(window, font=("PixelOperator8",40), text="Register", width=7)
@@ -153,11 +165,93 @@ def connected(database):
     r_conf_passw.place(x=338, y=229)
     r_conf_password = Entry(window, show='*', width=20)
     r_conf_password.place(x=435, y=237)
+    #email
+    mail = Label(window, font=("PixelOperator8",10), text="Adresse\nmail :", width=9)
+    mail.place(x=338, y=283)
+    mail_entry = Entry(window, width=20)
+    mail_entry.place(x=435, y=290)
     #Bouton
     register = Button(window, text="Register", command=register, width=10)
-    register.place(x=425, y=400)
+    register.place(x=425, y=430)
     window.update()
     window.mainloop()
+
+def mdp(database, win):
+    def check(*arg):
+        pseudo = pseudo_entry.get()
+        mail = email_entry.get()
+        if pseudo and mail :
+            if check_pseudo(pseudo):
+                if check_mail(mail, pseudo): 
+                    send_mail(mail, pseudo)
+                    done = Label(window, font=("PixelOperator8",40), text="Mail envoyé", background="black", fg="white")
+                    done.pack()
+                    window.update()
+                    done.place(x=300-(done.winfo_width()/2), y=350)
+                else:
+                    print("Adresse mail non valide")
+            else:
+               print("Ce pseudo n'existe pas") 
+        else:
+            print("Veuillez renseigner tout les champs")
+            
+                    
+    def send_mail(mail, pseudo):
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        username = 'mat33360@@gmail.com'
+        password = '20106969Aa'
+        server.starttls()
+        server.login(username,password)
+        msg = email.mime.text.MIMEText(f'Une demande de réinitialisation du mot de passe pour le compte "{pseudo}" à été demandé.'
+                                       + u'<br><a href="https://pynetgamechangepassword.000webhostapp.com?{}">Suivez ce lien pour le réinitialiser</a>'.format("pseudo="+pseudo+"&error=None"),'html')
+
+        msg['Subject'] = 'Réinitialisation de votre mot de passe'
+        msg['From'] = 'PyNetGame'
+        msg['To'] = pseudo
+        server.sendmail('PyNetGame@gmail.com', mail, msg.as_string())
+        server.quit()
+    
+    def check_pseudo(pseudo):
+        try:
+            query = database.get(f"SELECT user_name FROM users")
+            return (pseudo,) in query
+        except:
+            return False
+
+    def check_mail(mail,pseudo):
+        try:
+            query = database.get(f"SELECT email FROM users WHERE user_name = '{pseudo}'")[0]
+            return (mail,) == query
+        except:
+            return False
+        
+    win.destroy()
+    window = Tk()
+    window.title("PyNetGames")
+    window.geometry(f"{width}x{width}")
+    window.configure(background="black")
+    window.bind('<Return>', check)
+    window.protocol("WM_DELETE_WINDOW", lambda win = window : launch(win))
+  
+    title = Label(window, font=("PixelOperator8",40), text="Réinitialisation du\nmot de passe", background="black", fg="white")
+    title.pack()
+    window.update()
+    title.place(x=300-(title.winfo_width()/2), y=0)
+    
+    pseudo = Label(window, font=("PixelOperator8",10), text="Username :", width=9)
+    pseudo.place(x=200, y=180)
+    pseudo_entry = Entry(window, width=20)
+    pseudo_entry.place(x=300, y=183)
+
+    email_lab = Label(window, font=("PixelOperator8",10), text="Mail :", width=9)
+    email_lab.place(x=200, y=230)
+    email_entry = Entry(window, width=20)
+    email_entry.place(x=300, y=233)
+
+    confirm = Button(window, text="Confirm", command=check, width=10)
+    confirm.pack()
+    window.update()
+    confirm.place(x=300-(confirm.winfo_width()/2), y=283)
 
 def error():
     window = Tk()
