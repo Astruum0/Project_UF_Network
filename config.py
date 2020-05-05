556c4e072fa7ab35240ab55d4e3cc759370a32c4
arthur.vella@ynov.com <arthur.vella@ynov.com>
2020-05-05 17:02:43 +0200

    create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+help_msg = """
+PyNetGames 1.0
+
+Server configuration:
+
+    USAGE: server.py [configs] [save config]
+
+        -ip [IP Adress] : Give manual IP address to the server (Default is your device IP address)
+        -p [port] : Give manual port to the server (Default is 5555)
+        -s : Save this configuration to default value for next time"""
